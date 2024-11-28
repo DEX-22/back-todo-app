8eb0a414e9d5856ff103a88c8d8b34cfbbb391c4
dexx <enriquesaenz0@gmail.com>
2024-11-28 16:33:21 -0500

    refactor adding services

--- routes/task.route.js
+++ routes/task.route.js
@@ -1,83 +1,76 @@
-const express = require( 'express')
-const { validateRequest,properties } = require( '../middlewares/index.middleware.js')
-const { createTaskSchema,updateTaskSchema,getTaskSchema } = require( '../schemas/task.schema.js')
-const {models} = require("../libs/sequelize.lib.js")
+const express = require('express')
+const { validateRequest, properties } = require('../middlewares/index.middleware.js')
+const { createTaskSchema, updateTaskSchema, getTaskSchema } = require('../schemas/task.schema.js')
 const router = express.Router()
-const boom = require("@hapi/boom")
 
+const TaskService = require("../services/task.service.js")
+const service = new TaskService()
 
 router.get("/",
-    async (req,res,next)=>{
+    async (req, res, next) => {
         try {
-            const data = await models.Task.findAll()
-            
+            const data = await service.findAll()
+
             res.json(data)
         } catch (error) {
             next(error)
         }
     })
 router.post("/",
-    validateRequest(createTaskSchema,properties.body),    
-    async (req,res,next)=>{
-        const PENDING = "pending"
+    validateRequest(createTaskSchema, properties.body),
+    async (req, res, next) => {
+        try {
+            const body = req.body
 
-        const body = req.body
-        const newTask = await models.Task.create({...body,status: PENDING})
-        
-        res.json(newTask)
-})
+            const newTask = await service.create(body)
+
+            res.json(newTask)
+        } catch (error) {
+            next(error)
+        }
+    })
 router.get("/:id",
-validateRequest(getTaskSchema,properties.params),    
-async (req,res,next)=>{
-    try {
-        const id = req.params.id
-        const data = await models.Task.findByPk(id)
-        
-        if(!data) throw boom.notFound("Task not found")
+    validateRequest(getTaskSchema, properties.params),
+    async (req, res, next) => {
+        try {
+            const id = req.params.id
+            const data = await service.findOne(id)
 
-        res.json(data)
+            res.json(data)
 
-    } catch (error) {
-        next(error)
-    }
-})
+        } catch (error) {
+            next(error)
+        }
+    })
 router.patch("/:id",
-validateRequest(getTaskSchema,properties.params),
-validateRequest(updateTaskSchema,properties.body),
-async (req,res,next)=>{
-    try {
-        const id = req.params.id
-        const data = await models.Task.findByPk(id)
-        
-        if(!data) throw boom.notFound("Task not found") 
-        const changed = req.body
-        const updated = await data.update({
-            ...changed, updatedAt: new Date()
-        })
+    validateRequest(getTaskSchema, properties.params),
+    validateRequest(updateTaskSchema, properties.body),
+    async (req, res, next) => {
+        try {
+            const id = req.params.id
+            const changed = req.body
 
-        res.json(updated)
+            const updated = await service.update(id, changed)
 
-    } catch (error) {
-        next(error)
-    }
-})
+            res.json(updated)
+
+        } catch (error) {
+            next(error)
+        }
+    })
 router.delete("/:id",
-validateRequest(getTaskSchema,properties.params),    
-async (req,res,next)=>{
-    try {
-        const id = req.params.id
-        const data = await models.Task.findByPk(id)
-        
-        if(!data) throw boom.notFound("Task not found") 
-        const changed = req.body
-        const deleted = await data.destroy(id)
+    validateRequest(getTaskSchema, properties.params),
+    async (req, res, next) => {
+        try {
+            const id = req.params.id
+            const deleted = await service.delete(id)
 
-        res.json(deleted)
+            res.json(deleted)
 
-    } catch (error) {
-        next(error)
-    }
-})
+        } catch (error) {
+            next(error)
+        }
+    })
 
 
 module.exports = router
