9261eb722b13ec71cdc4da3a6eff024f0993a37c
dexx <enriquesaenz0@gmail.com>
2024-11-29 13:02:38 -0500

    fixing

--- libs/sequelize.lib.js
+++ libs/sequelize.lib.js
@@ -1,6 +1,7 @@
 const {Sequelize}= require('sequelize')
 const setupModels = require('../db/models/index.model.js')
 const config = require('../config/config.js')
+const pg = require("pg")
 
 const USER = encodeURIComponent(config.dbUser)
 const PASSWORD = encodeURIComponent(config.dbPassword)
@@ -9,6 +10,7 @@ const connectionString = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${conf
 const sqlize = new Sequelize(connectionString,
     {dialect:'postgres',
     dialectOptions: {
+            module:  pg,
         timezone: '+05:00' // Peru
         }
     })
