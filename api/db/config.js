40b4247f09b49e90bb1f0f5700c57f76ae37c88a
dexx <enriquesaenz0@gmail.com>
2024-11-29 10:32:20 -0500

    fixing actions to run migrations

--- api/db/config.js
+++ api/db/config.js
@@ -3,8 +3,7 @@ const config = require('../config/config.js')
 
 const USER = encodeURIComponent(config.dbUser)
 const PASSWORD = encodeURIComponent(config.dbPassword)
-const connectionString = config.connectionString ||
-`postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
+const connectionString = `postgres://${config.dbUser}:${config.dbPassword}@${config.dbHost}:${config.dbPort}/${config.dbName}`
 
 const sqlize = new Sequelize(connectionString,{dialect:'postgres'})
 
